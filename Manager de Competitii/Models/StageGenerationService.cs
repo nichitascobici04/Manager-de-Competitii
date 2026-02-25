e3374971c172e4f7d432fd1e391d3f4014909c2e
nichitascobici04 <nichita.scobici04@gmail.com>
2026-02-25 09:09:32 +0200

    Add StageGenerationService

---
+++ Manager de Competitii/Models/StageGenerationService.cs
@@ -0,0 +1,13 @@
+﻿using ManagerDeCompetitii.Models.MatchSet;
+
+namespace Manager_de_Competitii.Models
+{
+    public class StageGenerationService
+    {
+        public List<MatchSet> GenerateStages(List<Match> m)
+        {
+            // Logic to generate stages based on the matches
+            return new List<MatchSet>();
+        }
+    }
+}
