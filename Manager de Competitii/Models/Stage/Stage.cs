dfbfa3b13020c3f4eedb77b8dfd633ad3f171b52
nichitascobici04 <nichita.scobici04@gmail.com>
2026-03-11 08:18:01 +0200

    Added the Stages class

---
+++ Manager de Competitii/Models/Stage/Stage.cs
@@ -0,0 +1,10 @@
+﻿namespace Manager_de_Competitii.Models.Stage
+{
+    public class Stage
+    {
+        public int Id { get; set; }
+        public int TournamentId { get; set; }
+        public string Name { get; set; }
+        public List<Match> Matches { get; set; }
+    }
+}
