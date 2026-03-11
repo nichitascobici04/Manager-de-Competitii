b60acf9eaa75430a3927ca10a2f1dc1f4c303eb4
nichitascobici04 <nichita.scobici04@gmail.com>
2026-03-11 10:08:01 +0200

    Added the Round class

---
+++ Manager de Competitii/Models/Round/Round.cs
@@ -0,0 +1,9 @@
+﻿namespace Manager_de_Competitii.Models
+{
+    public class Round
+    {
+        public int Id { get; set; }
+        public List<Participant> Participants { get; set; }
+        public List<int> Scores { get; set; }
+    }
+}
